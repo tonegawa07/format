b2c7dc48cfb12f7614580e8788a2bcd73892e652
tonegawa07 <fukudayusuke07@gmail.com>
2020-12-04 10:55:00 +0900

    add grepl function

--- HPLC_format.R
+++ HPLC_format.R
@@ -15,14 +15,15 @@ cal_hplc = function(std_conc, Dil, Ext_vol, file_name){
   rawdata <- read.csv(paste0(file_name, ".csv"))
   
   # colname
-  names(rawdata)[c(1,2,ncol(rawdata))] <- c("label", "n", "DW")
+  names(rawdata)[c(1,ncol(rawdata))] <- c("label", "DW")
   
   # show std data
   std_area <-
     rawdata %>%
     select(1:(ncol(rawdata)-1)) %>%
-    filter(label == "STD") %>%
-    select(3:(ncol(rawdata)-1))
+    filter(grepl("STD", label)) %>%
+    #filter(label == "STD") %>%
+    select(2:(ncol(rawdata)-1))
   
   boxplot(std_area)
   
@@ -31,8 +32,9 @@ cal_hplc = function(std_conc, Dil, Ext_vol, file_name){
   std_mean <-
     rawdata %>%
     select(1:(ncol(rawdata)-1)) %>%
-    filter(label == "STD") %>%
-    gather(key = key, value = value, 3:(ncol(rawdata)-1), factor_key = T) %>%
+    filter(grepl("STD", label)) %>%
+    #filter(label == "STD") %>%
+    gather(key = key, value = value, 2:(ncol(rawdata)-1), factor_key = T) %>%
     group_by(key) %>%
     summarise(Mean = mean(value)) %>%
     spread(key = key, value = Mean)
@@ -44,9 +46,10 @@ cal_hplc = function(std_conc, Dil, Ext_vol, file_name){
   # Calculation sample_conc(mg/g)
   sample <-
     rawdata %>%
-    filter(label == "UNK")
+    filter(grepl("UNK", label))
+    #filter(label == "UNK")
   
-  for (i in 3:(ncol(sample)-1)) {
+  for (i in 2:(ncol(sample)-1)) {
     item <- colnames(sample[i])
     #print(item)
     col <- paste0(item, "(mg/g DW)")
@@ -65,5 +68,5 @@ cal_hplc = function(std_conc, Dil, Ext_vol, file_name){
   write.csv(sample, output_name, quote=F, row.names=F)
 }
 
-cal_hplc(std_conc = 10, Dil = 8, Ext_vol = 5, file_name = "Catechins_example")
+cal_hplc(std_conc = 10, Dil = 8, Ext_vol = 5, file_name = "Catechins_example_grepl")
 cal_hplc(std_conc = 10, Dil = 1.25, Ext_vol = 5, file_name = "FAAs_example")
